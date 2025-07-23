0ee71aafccfca7199629f3fb258ab516739577fc
zarazahed <zara.layla.zahed@gmail.com>
2025-07-23 01:06:52 -0500

    add sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 PORT"
+  exit 1
+fi
+
+PORT="$1"
+
+if ! [[ "$PORT" =~ ^[0-9]+$ ]]; then
+  echo "PORT must be a non-negative integer."
+  exit 1
+fi
+
+export RAILS_ENV=production
+
+if [ -z "$SECRET_KEY_BASE" ]; then
+  echo "SECRET_KEY_BASE not set, generating one."
+  export SECRET_KEY_BASE=$(bin/rails secret)
+fi
+
+PIDS=$(timeout 2s lsof -ti ":$PORT")
+if [ -n "$PIDS" ]; then
+  kill -9 $PIDS
+fi
+
+git pull
+bundle config set --local path 'vendor/bundle'
+bundle install
+bin/rails db:migrate
+bin/rails assets:precompile
+bin/rails server -p "$PORT"
