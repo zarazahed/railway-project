a88ed2358b365ab34d6060989ed51e8e34e7a2f1
zarazahed <zara.layla.zahed@gmail.com>
2025-07-23 01:53:16 -0500

    comment

--- setup.sh
+++ setup.sh
@@ -12,11 +12,11 @@ fi
 
 export RAILS_ENV=production
 
-if [ -z "$SECRET_KEY_BASE" ]; then
-  echo "SECRET_KEY_BASE not set, generating one."
-  export SECRET_KEY_BASE=$(bin/rails secret)
-  echo "...: $SECRET_KEY_BASE"
-fi
+# if [ -z "$SECRET_KEY_BASE" ]; then
+#   echo "SECRET_KEY_BASE not set, generating one."
+#   export SECRET_KEY_BASE=$(bin/rails secret)
+#   echo "...: $SECRET_KEY_BASE"
+# fi
 
 PIDS=$(timeout 2s lsof -ti ":$PORT")
 if [ -n "$PIDS" ]; then
