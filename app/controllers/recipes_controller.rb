e24a89d2385edc079f84237daa1d5fbfb8a518c5
zarazahed <zara.layla.zahed@gmail.com>
2025-07-23 23:09:47 -0500

    Fix delete recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,7 +8,8 @@ class RecipesController < ApplicationController
 
   # GET /recipes/1 or /recipes/1.json
   def show
-  end
+  @recipe = Recipe.find(params[:id])
+end
 
   # GET /recipes/new
   def new
